8e75ffd0bc54c21973ec854a58006bb81251d4b5
Nehoray Peretz <91248841+nehorayp@users.noreply.github.com>
2023-12-17 15:19:08 +0200

    Fixed an issue when user enters invalid key format
    An issue when a user enters a key that is not a valid hexadecimal number.

--- main.py
+++ main.py
@@ -15,7 +15,12 @@ KEY_FILE_PATH = ""
 
 
 def show_decrypted_text(decrypted_text):
-    # Function to create a new window and display decrypted text
+    """
+    Function to create a new window and display decrypted text.
+
+    Args:
+        decrypted_text (str): The decrypted text to be displayed.
+    """
     new_window = tk.Toplevel(app)
     new_window.title("Decrypted Text")
 
@@ -25,11 +30,13 @@ def show_decrypted_text(decrypted_text):
 
 
 def create_aes_encryption():
-    # Placeholder function for AES encryption
+    """
+    Function to perform AES encryption on a selected file and save both the encrypted file and the encryption key.
+    """
     file_path = filedialog.askopenfilename(title="Select a file for AES encryption")
 
     if file_path:
-        aes_key = os.urandom(32)  # In a real-world scenario, you should securely manage and store the key
+        aes_key = os.urandom(32)
 
         with open(file_path, 'rb') as file:
             plaintext = file.read()
@@ -43,18 +50,24 @@ def create_aes_encryption():
         ciphertext = encryptor.update(padded_data) + encryptor.finalize()
 
         # Ask the user where to save the encrypted file
-        encrypted_file_path = filedialog.asksaveasfilename(title="Save the encrypted file",
-                                                            defaultextension=".enc",
-                                                            filetypes=[("Encrypted Files", "*.enc")])
+        encrypted_file_path = filedialog.asksaveasfilename(
+            title="Save the encrypted file",
+            defaultextension=".enc",
+            filetypes=[("Encrypted Files", "*.enc")]
+        )
+
         if encrypted_file_path:
             with open(encrypted_file_path, 'wb') as file:
                 file.write(ciphertext)
 
             # Ask the user where to save the key
             global KEY_FILE_PATH
-            key_file_path = filedialog.asksaveasfilename(title="Save the encryption key",
-                                                          defaultextension=".txt",
-                                                          filetypes=[("Text Files", "*.txt")])
+            key_file_path = filedialog.asksaveasfilename(
+                title="Save the encryption key",
+                defaultextension=".txt",
+                filetypes=[("Text Files", "*.txt")]
+            )
+
             if key_file_path:
                 with open(key_file_path, 'w') as key_file:
                     key_file.write(aes_key.hex())
@@ -64,47 +77,53 @@ def create_aes_encryption():
 
 
 def decrypt_aes_file():
-    # Placeholder function for AES decryption
+    """
+    Function to perform AES decryption on a selected file using a user-provided key and display the decrypted text.
+    """
     file_path = filedialog.askopenfilename(title="Select a file for AES decryption")
 
     if file_path:
-        user_key = simpledialog.askstring("Key Input", "Enter the encryption key:")
+        while True:
+            user_key = simpledialog.askstring("Key Input", "Enter the encryption key:")
 
-        if user_key is None:
-            # User canceled key entry
-            return
+            if user_key is None:
+                # User canceled key entry
+                return
 
-        aes_key = bytes.fromhex(user_key)
+            try:
+                aes_key = bytes.fromhex(user_key)
 
-        try:
-            with open(file_path, 'rb') as file:
-                ciphertext = file.read()
+                with open(file_path, 'rb') as file:
+                    ciphertext = file.read()
 
-            cipher = Cipher(algorithms.AES(aes_key), modes.ECB(), backend=default_backend())
+                cipher = Cipher(algorithms.AES(aes_key), modes.ECB(), backend=default_backend())
 
-            decryptor = cipher.decryptor()
-            decrypted_data = decryptor.update(ciphertext) + decryptor.finalize()
+                decryptor = cipher.decryptor()
+                decrypted_data = decryptor.update(ciphertext) + decryptor.finalize()
 
-            # Remove PKCS7 padding
-            unpadder = sym_padding.PKCS7(algorithms.AES.block_size).unpadder()
-            plaintext = unpadder.update(decrypted_data) + unpadder.finalize()
+                unpadder = sym_padding.PKCS7(algorithms.AES.block_size).unpadder()
+                plaintext = unpadder.update(decrypted_data) + unpadder.finalize()
 
-            # Decode the plaintext, handling decoding errors
-            decoded_text = plaintext.decode('utf-8', errors='replace')
+                decoded_text = plaintext.decode('utf-8', errors='replace')
 
-            # Display the decrypted text in a new window
-            show_decrypted_text(decoded_text)
+                show_decrypted_text(decoded_text)
 
-            messagebox.showinfo("Decryption Complete", "AES Decryption has been completed.")
-        except ValueError:
-            # If ValueError occurs, it might be due to incorrect padding
-            messagebox.showerror("Decryption Error", "AES Decryption failed. Incorrect padding or key.")
+                messagebox.showinfo("Decryption Complete", "AES Decryption has been completed.")
+                break  # Break out of the loop if decryption is successful
+            except ValueError:
+                messagebox.showerror("Decryption Error", "Wrong key or invalid key format. Please enter a valid key")
+            except Exception as e:
+                messagebox.showerror("Error", f"An error occurred: {e}")
 
-        except Exception as e:
-            messagebox.showerror("Error", f"An error occurred: {e}")
 
 
 def create_exit_button(main_window):
+    """
+    Function to create an Exit button in the main window.
+
+    Args:
+        main_window: The main window where the button will be placed.
+    """
     exit_button = tk.Button(
         master=main_window,
         text="Exit",
@@ -116,8 +135,11 @@ def create_exit_button(main_window):
 
 
 def create_main_window():
+    """
+    Function to create the main window with buttons for AES encryption, AES decryption, and an Exit button.
+    """
     main_window = tk.Toplevel(app)
-    main_window.title("Main Window")
+    main_window.title("Main Menu")
 
     aes_button = tk.Button(
         master=main_window,
@@ -141,6 +163,9 @@ def create_main_window():
 
 
 def create_initial_window():
+    """
+    Function to create the initial window with a welcome message and a Continue button.
+    """
     greet_frame = tk.Frame(app)
     greet_frame.pack(pady=50)
     description_frame = tk.Frame(app)
